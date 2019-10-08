2186223625f75821945d9a3faf39b2ae019a061f
Mirat Can Bayrak <miratcanbayrak@gmail.com>
2019-10-08 20:18:53 +0300

    Daily commit

--- logme.py
+++ logme.py
@@ -8,11 +8,39 @@ __copyright__ = "Copyright 2016, Planet Earth"
 
 from datetime import datetime
 
+import pickle
 from os import mkdir
 from os.path import expanduser, join, exists
 import argparse
 import subprocess
 from tempfile import NamedTemporaryFile
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+
+SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly']
+CONFIG_DIR = expanduser('~/.config/logme/')
+TOKEN_PATH = join(CONFIG_DIR, 'token.pickle')
+
+if not exists(CONFIG_DIR):
+    mkdir(CONFIG_DIR)
+
+def get_drive_service():
+    creds = None
+    if exists(TOKEN_PATH):
+        with open(TOKEN_PATH, 'rb') as token:
+            creds = pickle.load(token)
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        with open(TOKEN_PATH, 'wb') as token:
+            pickle.dump(creds, token)
+    return build('drive', 'v3', credentials=creds)
 
 
 def parse_args():
@@ -31,7 +59,7 @@ def parse_args():
 def get_story_from_editor():
     with NamedTemporaryFile('r') as tfile:
         subprocess.call(["vim", tfile.name])
-        with open(tfile.name, 'r') as sfile:  # TODO: Fix this hack.
+        with open(tfile.name, 'r') as sfile:
             story = sfile.read()
     return story
 
@@ -42,16 +70,33 @@ if args.use_editor:
 else:
     story = args.story
 
-DAY_FILE_PATTERN = '%Y-%m-%d.txt'
+DAY_FILE_PATTERN = '%m-%d.txt'
 DAY_TITLE_PATTERN = '%Y/%m/%d/ - %A\n'
-LOG_DIR = join(expanduser('~'), '.logme')
 
-if not exists(LOG_DIR):
-    mkdir(LOG_DIR)
-
+service = get_drive_service()
 now = datetime.now()
+REMOTE_APP_DIR =  'MyLogs'
+
 file_name = datetime.strftime(now, DAY_FILE_PATTERN)
-file_path = join(LOG_DIR, file_name)
+
+
+def get_or_create_remote_dir():
+    try:
+        root_folder = service.files().list(
+            pageSize=10, fields="files(id, name)",
+            q=f"mimeType='application/vnd.google-apps.folder' and " \
+               "name='{REMOTE_APP_DIR}' and trashed=false"
+        ).execute()['files'][0]['id']
+    except (KeyError, IndexError):
+        file_metadata = {
+            'name': REMOTE_APP_DIR,
+            'mimeType': 'application/vnd.google-apps.folder'
+        }
+        root_folder = service.files().create(
+            body=file_metadata, fields='id').execute()
+    return root_folder
+
+print(get_or_create_remote_dir())
 
 if not exists(file_path):
     fp = open(file_path, 'w')
